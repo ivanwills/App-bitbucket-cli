ce96ad42982370a4deee24165cdcae67d79d7846
Ivan Wills <Ivan.Wills2@optus.com.au>
2017-05-22 15:00:36 +1000

    Split logic into two modules

--- lib/App/BitBucketCli/Core.pm
+++ lib/App/BitBucketCli/Core.pm
@@ -1,4 +1,4 @@
-package App::BitBucketCli;
+package App::BitBucketCli::Core;
 
 # Created on: 2017-04-24 08:14:30
 # Create by:  Ivan Wills
@@ -14,44 +14,142 @@ use WWW::Mechanize;
 use JSON::XS qw/decode_json encode_json/;
 use Data::Dumper qw/Dumper/;
 use English qw/ -no_match_vars /;
-use App::BitBucketCli::Core;
+use App::BitBucketCli::Branch;
+use App::BitBucketCli::PullRequest;
 
 our $VERSION = 0.001;
 
-has core => (
-    is      => 'ro',
-    handles => [qw/
-        opt
-    /],
+has url => (
+    is      => 'rw',
+    builder => '_url',
+    lazy    => 1,
+);
+has host => (
+    is      => 'rw',
+    required => 1,
+);
+has [qw/user pass/] => (
+    is  => 'rw',
+);
+has mech => (
+    is      => 'rw',
+    default => sub { WWW::Mechanize->new },
+);
+has opt => (
+    is      => 'rw',
+    default => sub {{}},
 );
 
-around BUILDARGS => sub {
-    my ($orig, $class, @params) = @_;
-    my %param;
-
-    if ( ref $params[0] eq 'HASH' ) {
-        %param = %{ shift @params };
-    }
-    else {
-        %param = @params;
-    }
-
-    $param{core} = App::BitBucketCli::Core->new(%param);
-
-    return $class->$orig(%param);
-};
-
 sub projects {
-    my ($self, @filter) = @_;
+    my ($self) = @_;
+    my @projects;
+    my $last_page = 0;
+    my $next_page_start = 0;
+    my $limit = 30;
 
-    my @projects = sort {
-        $a->{name} cmd $b->{name};
+    while ( ! $last_page ) {
+        my $json;
+        eval {
+            $json = $self->_get($self->url . "/projects?limit=$limit&start=$next_page_start");
+            1;
+        } || do {
+            warn "Couldn't list repositories: $@\n";
+            return [];
+        };
+        push @projects, @{ $json->{values} };
+        $last_page = $json->{isLastPage};
+        $next_page_start = $json->{nextPageStart};
     }
-    $self->core->projects();
 
-    for my $project (@projects) {
-        print "$project->{name}\n";
+    die Dumper \@projects;
+}
+
+sub branch {
+    my ($self, @branches) = @_;
+
+    my $branches = $self->get_branches($self->opt->{project}, $self->opt->{repository});
+
+    for my $branch (sort keys %{ $branches }) {
+        next if !grep {$branch eq $_} @branches;
+        print "$branch\n";
     }
+
+    return;
+}
+
+sub get_pull_requests {
+    my ($self, $project, $repository) = @_;
+    my $json;
+    my @prs;
+
+    eval {
+        $json = $self->_get($self->url . "/projects/$project/repos/$repository/pull-requests");
+    };
+    if ($@) {
+        warn "Couldn't get pull requests for $project/$repository\n";
+        return [];
+    }
+
+    for my $pr (@{ $json->{values} }) {
+        push @prs, App::BitBucketCli::PullRequest->new($pr);
+    }
+
+    return \@prs;
+}
+
+sub get_branches {
+    my ($self, $project, $repository) = @_;
+    my $json;
+    my @branches;
+
+    eval {
+        $json = $self->_get($self->url . "/projects/$project/repos/$repository/branches?orderBy=MODIFICATION&details=true&limit=100");
+    };
+    my $error = $@;
+    if ($error) {
+        if ($error =~ /Unauthorized/) {
+            warn "Unauthorized, please try resetting your password!\n";
+            exit 10;
+        }
+
+        warn "Couldn't get branches of $project/$repository\n";
+        return [];
+    }
+
+    for my $branch (@{ $json->{values} }) {
+        $branch->{project}    = $project;
+        $branch->{repository} = $repository;
+        push @branches, App::BitBucketCli::Branch->new($branch);
+    }
+
+    return \@branches;
+}
+
+sub _get {
+    my ($self, $url) = @_;
+
+    $self->mech->get($url);
+
+    return decode_json($self->mech->content);
+}
+
+sub _url {
+    my ($self) = @_;
+    warn my $url = "https://"
+        . _url_encode($self->user)
+        . ':'
+        . _url_encode($self->pass)
+        . '@'
+        . $self->host
+        . "/rest/api/1.0";
+
+    return $url;
+}
+
+sub _url_encode {
+    my $str = shift;
+    $str =~ s/(\W)/sprintf('%%%x',ord($1))/eg;
+    return $str;
 }
 
 1;
@@ -60,19 +158,19 @@ __END__
 
 =head1 NAME
 
-App::BitBucketCli - Library for talking to BitBucket Server (or Stash)
+App::BitBucketCli::Core - Library for talking to BitBucket Server (or Stash)
 
 =head1 VERSION
 
-This documentation refers to App::BitBucketCli version 0.0.1
+This documentation refers to App::BitBucketCli::Core version 0.0.1
 
 
 =head1 SYNOPSIS
 
-   use App::BitBucketCli;
+   use App::BitBucketCli::Core;
 
    # create a stash object
-   my $stash = App::BitBucketCli->new(
+   my $stash = App::BitBucketCli::Core->new(
        url => 'http://stash.example.com/',
    );
 
@@ -105,7 +203,7 @@ context to help them understand the methods that are subsequently described.
 
 Param: C<$search> - type (detail) - description
 
-Return: App::BitBucketCli -
+Return: App::BitBucketCli::Core -
 
 Description:
 
